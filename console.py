039e1364f9b696ef12e90b93152fe564bc4b182a
Arnold Henry <devharnold@gmail.com>
2023-10-14 02:13:52 +0300

    late-night

--- console.py
+++ console.py
@@ -84,6 +84,60 @@ class HBNBCommand(cmd.Cmd):
             del objectdict["{}.{}".format(parsed_args[0], parsed_args[1])]
             models.storage.save()
 
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id
+        by adding or updating an attribute (save the changes into a JSON file).
+        Usage: update <class name> <id> <attribute name> "<attribute value>"
+        Only one attribute can be updated at a time.
+        You can assume the attribute name is valid (exists for this model).
+        The attribute value must be casted to the attribute type.
+        """
+        parsed_args = parse(arg)
+        objectdict = models.storage.all()
+
+        if len(parsed_args) < 2:
+            print("** Usage: update <class name> <id> <attribute name> \"<attribute value>\" **")
+            return False
+
+        class_name = parsed_args[0]
+        instance_id = parsed_args[1]
+
+        if class_name not in HBNBCommand.__classes:
+            print("** Class doesn't exist **")
+            return False
+
+        instance_key = "{}.{}".format(class_name, instance_id)
+
+        if instance_key not in objectdict.keys():
+            print("** No instance found **")
+            return False
+
+        if len(parsed_args) < 4:
+            print("** Attribute name or value missing **")
+            return False
+
+        attribute_name = parsed_args[2]
+        attribute_value = parsed_args[3]
+
+        if attribute_name not in objectdict[instance_key].__class__.__dict__.keys():
+            print(f"** Attribute '{attribute_name}' is not valid for this class **")
+            return False
+
+        attribute_type = type(objectdict[instance_key].__class__.__dict__[attribute_name])
+
+        try:
+            casted_value = attribute_type(attribute_value)
+        except (ValueError, TypeError):
+            print("** Attribute value could not be cast to the correct type **")
+            return False
+
+        setattr(objectdict[instance_key], attribute_name, casted_value)
+        models.storage.save()
+
+
+
+
 
 
 
