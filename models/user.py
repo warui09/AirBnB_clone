c84741b7017e5dcbf0abc369c96d5abc3783879e
Arnold Henry <devharnold@gmail.com>
2023-10-13 03:45:41 +0300

    user

---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""This file here will define a user class"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """In-depth definition of the class user
+    Attributes:
+    email (str): User's email
+    password (str): User's password
+    first_name (str): User's first name
+    last_name (str): User's last name
+    """
+    email = " "
+    password = " "
+    first_name = " "
+    last_name = " "
